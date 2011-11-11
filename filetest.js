b9d29482caaad7bf323077108229e7baa28ec2a5
Elliott Foster <elliottf@codebrews.com>
2011-11-11 12:23:23 -0600

    Updating compress.cc and test scripts to play nice with latest version of node.

--- filetest.js
+++ filetest.js
@@ -1,14 +1,14 @@
 var compress=require("./compress");
-var sys=require("sys");
-var posix=require("posix");
+var util=require("util");
+var fs=require("fs");
 
 // Read in our test file
-var data=posix.cat("filetest.js", encoding="binary").wait();
-sys.puts("Got : "+data.length);
+var data = fs.readFileSync("filetest.js", "binary");
+util.puts("Got : "+data.length);
 
 // Set output file
-var fd = posix.open("filetest.js.gz", process.O_WRONLY | process.O_TRUNC | process.O_CREAT, 0644).wait();
-sys.puts("Openned file");
+var fd = fs.openSync("filetest.js.gz", 'w', 0644);
+util.puts("Openned file");
 
 // Create gzip stream
 var gzip=new compress.Gzip;
@@ -16,22 +16,22 @@ gzip.init();
 
 // Pump data to be compressed
 gzdata=gzip.deflate(data, "binary");  // Do this as many times as required
-sys.puts("Compressed size : "+gzdata.length);
-posix.write(fd, gzdata, encoding="binary").wait();
+util.puts("Compressed size : "+gzdata.length);
+var pos = fs.writeSync(fd, gzdata, 0, "binary");
 
 // Get the last bit
 gzlast=gzip.end();
-sys.puts("Last bit : "+gzlast.length);
-posix.write(fd, gzlast, encoding="binary").wait();
-posix.close(fd).wait();
-sys.puts("File closed");
+util.puts("Last bit : "+gzlast.length);
+fs.writeSync(fd, gzlast, pos, "binary");
+fs.closeSync(fd);
+util.puts("File closed");
 
 // See if we can uncompress it ok
 var gunzip=new compress.Gunzip;
 gunzip.init();
-var testdata = posix.cat("filetest.js.gz", encoding="binary").wait();
-sys.puts("Test opened : "+testdata.length);
-sys.puts(gunzip.inflate(testdata, "binary").length);
+var testdata = fs.readFileSync("filetest.js.gz", "binary");
+util.puts("Test opened : "+testdata.length);
+util.puts(gunzip.inflate(testdata, "binary").length);
 gunzip.end();
 
 
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var compress=require("./compress");
-var sys=require("sys");
-var posix=require("posix");
+var util=require("util");
+var fs=require("fs");
 
 // Create gzip stream
 var gzip=new compress.Gzip;
@@ -8,17 +8,17 @@ gzip.init();
 
 // Pump data to be compressed
 var gzdata1 = gzip.deflate("My data that needs ", "binary"); 
-sys.puts("Compressed size : "+gzdata1.length);
+util.puts("Compressed size : "+gzdata1.length);
 
 var gzdata2 = gzip.deflate("to be compressed. 01234567890.", "binary"); 
-sys.puts("Compressed size : "+gzdata2.length);
+util.puts("Compressed size : "+gzdata2.length);
 
 var gzdata3=gzip.end();
-sys.puts("Last bit : "+gzdata3.length);
+util.puts("Last bit : "+gzdata3.length);
 
 // Take the output stream, and chop it up into two
 var gzdata = gzdata1+gzdata2+gzdata3;
-sys.puts("Total compressed size : "+gzdata.length);
+util.puts("Total compressed size : "+gzdata.length);
 var d1 = gzdata.substr(0, 25);
 var d2 = gzdata.substr(25);
 
@@ -29,7 +29,7 @@ var data1 = gunzip.inflate(d1, "binary");
 var data2 = gunzip.inflate(d2, "binary");
 var data3 = gunzip.end();
 
-sys.puts(data1+data2+data3);
+util.puts(data1+data2+data3);
 
 
 
